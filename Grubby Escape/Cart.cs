63f9654b8890e0f5f71889cbc9eb3bfa3fe6e3ef
Emmett Cornies <339761330@GoTVDSB.ca>
2025-11-06 16:39:17 -0500

    working on cart class

---
+++ Grubby Escape/Cart.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grubby_Escape
+{
+    internal class Cart
+    {
+        private Rectangle _hitbox;
+        private Vector2 _position;
+        private Texture2D _cartTex;
+        private Texture2D _wheelTex;
+        private float _rotationAmt;
+
+        public Cart(Texture2D cartTexture, Texture2D wheelTexture, Vector2 startingPos)
+        {
+            _cartTex = cartTexture;
+            _wheelTex = wheelTexture;
+            _position = startingPos;
+
+            _hitbox = new Rectangle();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _hitbox.X = (int)_position.X;
+            _hitbox.Y = (int)_position.Y;
+        }
+    }
+}
