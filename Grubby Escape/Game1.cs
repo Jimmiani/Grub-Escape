facb5e9e13b055bd0cd6be2e7314f37395272d6c
Emmett Cornies <339761330@GoTVDSB.ca>
2025-11-17 23:09:59 -0500

    Work on the math

--- Grubby Escape/Game1.cs
+++ Grubby Escape/Game1.cs
@@ -129,7 +129,7 @@ namespace Grubby_Escape
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
         }
 
         protected override void Initialize()
